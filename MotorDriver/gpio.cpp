edbf77de42bfd27696ae4492c54b82a033c8a210
Chuck Dickerson <cjdicker@mtu.edu>
2021-03-12 17:00:43 -0500

    Now working in BBB using Eclipse

---
+++ MotorDriver/gpio.cpp
@@ -0,0 +1,87 @@
+/*
+ * gpio.cpp
+ *
+ *  Created on: Mar 12, 2021
+ *      Author: chuck
+ */
+#include "gpio.h"
+#include "pwm.h"
+#include <stdlib.h>
+
+using namespace std;
+
+namespace myNameSpace {
+	// constructor
+GPIO::GPIO(string pin,int number) {
+	this->number = number;
+	string name = "gpio";
+	name += to_string(number);
+	this->name = name;
+	this->path = GPIO_PATH + this->name + "/";
+	this->muxPath = GPIO_PINMUX_PATH + pin + "_pinmux/";
+}
+
+string GPIO::getPinState() {
+	string state = PWM::readFile(this->muxPath, PIN_STATE);
+	return state;
+}
+
+int GPIO::setPinState(string value) {
+	return PWM::writeFile(this->muxPath, PIN_STATE, value);
+}
+
+GPIO_DIRECTION GPIO::getDirection() {
+	string input = PWM::readFile(this->path, "direction");
+	if (input == "in")
+		return INPUT;
+	else return OUTPUT;
+}
+
+GPIO_VALUE GPIO::getValue() {
+	string input = PWM::readFile(this->path, "value");
+	if (input == "0") return LOW;
+	else return HIGH;
+}
+
+int GPIO::setDirection(GPIO_DIRECTION dir) {
+	switch (dir) {
+	case INPUT: return PWM::writeFile(this->path, "direction", "in");
+		break;
+	case OUTPUT: return PWM::writeFile(this->path, "direction", "out");
+		break;
+	}
+	return -1;
+}
+
+int GPIO::setValue(GPIO_VALUE value) {
+	switch (value) {
+	case LOW: return PWM::writeFile(this->path, "value", "0");
+		break;
+	case HIGH: return PWM::writeFile(this->path, "value", "1");
+		break;
+	}
+	return -1;
+}
+
+int GPIO::toggleValue() {
+	GPIO_VALUE value = this->getValue();
+	if (value == HIGH)
+		this->setValue(LOW);
+	else
+		this->setValue(HIGH);
+	return 0;
+}
+
+int GPIO::getNumber() {
+	return this->number;
+}
+
+GPIO::~GPIO() {}
+
+}
+
+
+
+
+
+
---
+++ MotorDriver/pwmMotor.cpp
@@ -0,0 +1,89 @@
+/*
+ * pwmMotor.cpp
+ *
+ *  Created on: Mar 12, 2021
+ *      Author: chuck
+ */
+/*
+ * pwmMotor.c++
+ *
+ *  Created on: Feb 27, 2021
+ *      Author: chuck
+ */
+#include "pwm.h"
+#include "gpio.h"
+
+#include <iostream>
+#include <unistd.h>
+
+using namespace myNameSpace;
+using namespace std;
+
+int main() {
+	string path;
+	string pin_state;
+
+	cout << endl;
+	cout << "PWM Program for PWM1A" << endl;
+
+	// constructors
+	PWM pwm;
+	GPIO P9_15("P9_15",48);
+	GPIO P9_17("P9_17",5);
+
+	cout << "Setup PWM and GPIO pins" << endl;
+
+	// Set state of P9.14 to pwm
+	pwm.setPWMPinState("pwm");
+
+	// Set state of P9.15 (gpio 48) and P9.17 (gpio 5) to gpio
+	P9_15.setPinState("gpio");
+	P9_17.setPinState("gpio");
+
+	// Set direction of P9.15 (gpio 48) and P9.17 (gpio 5) to out
+	P9_15.setDirection(OUTPUT);
+	P9_17.setDirection(OUTPUT);
+
+	// set initial motor direction
+	pwm.setMotorDirection(CW, P9_15.getNumber(), P9_17.getNumber());
+
+	// setup PWM
+	pwm.setPeriod(50000);
+	pwm.setDutyCyclePct(0.0f);
+
+	pwm.enablePWM();
+	cout << "Enable PWM" << endl;
+
+	//=====================start running motor==============================
+
+	cout << "Start CW Ramp" << endl;
+
+	// ramp to 90% over 5 seconds
+	pwm.rampSpeed(90.0f,5);
+
+	// ramp to 0% over 5 seconds
+	pwm.rampSpeed(0.0f,5);
+
+	// Change motor direction
+	pwm.setMotorDirection(CCW, P9_15.getNumber(), P9_17.getNumber());
+	cout << "Change motor direction " << endl;
+
+	cout << "Start CCW Ramp" << endl;
+
+	// ramp to 90% over 3 seconds
+	pwm.rampSpeed(90.0f,2);
+
+	// ramp to 0% over 3 seconds
+	pwm.rampSpeed(0.0f,2);
+
+	// Set inputs to stop, turn off PWM when done
+	pwm.setMotorDirection(STOP, P9_15.getNumber(), P9_17.getNumber());
+	pwm.disablePWM();
+	cout << "Disable PWM, stop motor " << endl;
+}
+
+
+
+
+
+
